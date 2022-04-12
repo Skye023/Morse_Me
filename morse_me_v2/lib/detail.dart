82696ba8cfb3eb50fd0a3c259b5f93fe8844c812
Skye023 <Skye023@users.noreply.github.com>
2022-04-12 14:50:46 +0700

    NEW VERSION IS COMING!

---
+++ morse_me_v2/lib/detail.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'overview.dart';
+
+class MorseDetail extends StatefulWidget {
+  final Info info;
+  const MorseDetail({Key? key, required this.info}) : super(key: key);
+  @override
+  _MorseDetailState createState() {
+    return _MorseDetailState();
+  }
+}
+
+class _MorseDetailState extends State<MorseDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.info.label),
+          backgroundColor: Colors.greenAccent,
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(children: <Widget>[
+                SizedBox(
+                    height: 400,
+                    width: double.infinity,
+                    child: Image(image: AssetImage(widget.info.imgURL))),
+                const SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  widget.info.info,
+                  style: const TextStyle(color: Colors.teal, fontSize: 18),
+                ),
+              ])),
+        ));
+  }
+}
---
+++ morse_me_v2/lib/main.dart
@@ -0,0 +1,125 @@
+import 'dart:html';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+import 'overview.dart';
+import 'detail.dart';
+
+void main() => runApp(const Morse_Me());
+
+class Morse_Me extends StatelessWidget {
+  const Morse_Me({Key? key}) : super(key: key);
+
+  static const String _title = 'Morse Me!';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static final List<ListView> _widgetOptions = <ListView>[
+    ListView.builder(
+      itemCount: Info.inf.length,
+      itemBuilder: (BuildContext context, int index) {
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return MorseDetail(info: Info.inf[index]);
+            }));
+          },
+          child: buildInfoCard(Info.inf[index]),
+        );
+      },
+    ),
+    ListView.builder(
+      itemCount: Info.inf.length,
+      itemBuilder: (BuildContext context, int index) {
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return MorseDetail(info: Info.inf[index]);
+            }));
+          },
+          child: buildInfoCard(Info.inf[index]),
+        );
+      },
+    ),
+  ];
+
+  static Widget buildInfoCard(Info info) {
+    return Card(
+      elevation: 2.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(children: <Widget>[
+            SizedBox(
+              height: 200,
+              child: Image(image: AssetImage(info.imgURL)),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              info.label,
+              style: const TextStyle(
+                  color: Colors.blueGrey,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Palatino'),
+            )
+          ])),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Morse Me!'),
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.keyboard_outlined),
+            label: 'Converter',
+          ),
+        ],
+        backgroundColor: Colors.yellow[400],
+        unselectedItemColor: Colors.white,
+        selectedFontSize: 16.0,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.black,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
